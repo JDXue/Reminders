29e34f2856a2a6ccd24f710b65a36ed7ed5807a7
Jess Dam <Jess Dam>
2019-11-12 15:41:04 +0000

    toggle allows only reminders for today to show, still ficxing no date circumstance

---
+++ src/components/AllReminders.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import './Reminders.css'
+import '../App.css'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button'
+import { useDispatch, useSelector } from 'react-redux'
+import { addToReminders, removeFromReminders } from '../redux'
+
+
+function AllReminders(){
+    const remindersState  = useSelector(storeState => storeState.myReminders)
+    const dispatch = useDispatch()
+
+    console.log(remindersState)
+
+
+    return(
+        <>
+            {
+                remindersState
+                    .filter(reminder => reminder.length > 1)
+                    .map((reminder, index) =>
+                        <div className="reminder-box" key={index + reminder}>
+                            <p className='reminder'>
+                                <b>{reminder[1].toLocaleDateString('en-GB',{timeZone: 'GMT', dateStyle: 'medium'})}</b>  {reminder[0]}
+                            </p>
+                            <Button
+                                id={index}
+                                className='delete-button'
+                                variant='light'
+                                onClick= {()=>{
+                                    console.log(index)
+                                    console.log('removed a reminder')
+                                    dispatch(removeFromReminders(index))
+                                }}
+                            >X</Button>
+                        </div>
+                    )
+
+            }
+            <br></br>
+
+
+            <h2>Other Reminders</h2>
+
+            {
+                remindersState
+                .filter(reminder => reminder.length = 1)
+                .map((reminder, index) =>
+                    <div className="reminder-box" key={index + reminder}>
+                        <p className='reminder'>{reminder[0]} </p>
+                        <Button
+                            id={index}
+                            className='delete-button'
+                            variant='light'
+                            onClick= {()=>{
+                                console.log(index)
+                                console.log('removed a reminder')
+                                dispatch(removeFromReminders(index))
+                            }}
+                        >X</Button>
+                    </div>
+                )
+            }
+        </>
+    )
+}
+
+export default AllReminders
--- src/components/CurrentReminders.js
+++ src/components/CurrentReminders.js
@@ -9,34 +9,23 @@ import { useDispatch, useSelector } from 'react-redux'
 import { addToReminders, removeFromReminders } from '../redux'
 import './Reminders.css'
 
+import RemindersToday from './RemindersToday'
+import AllReminders from './AllReminders'
 import ToggleReminders from './ToggleReminders'
 
 
 function CurrentReminders(){
-    const remindersState  = useSelector(storeState => storeState.myReminders)
     const justRemindersToday = useSelector(storeState => storeState.onlyShowRemindersToday)
-    const dispatch = useDispatch()
-    console.log(remindersState)
-    console.log(justRemindersToday)
+
     return(
         <div>
             <div className='reminders-container'>
                 {
-                    remindersState.map((reminder, index) =>
-                        <div className="reminder-box" key={index + reminder}>
-                            <p className='reminder'>{reminder[1].toLocaleDateString('en-GB')} {reminder[0]} </p>
-                            <Button
-                                id={index}
-                                className='delete-button'
-                                variant='light'
-                                onClick= {()=>{
-                                    console.log(index)
-                                    console.log('removed a reminder')
-                                    dispatch(removeFromReminders(index))
-                                }}
-                            >X</Button>
-                        </div>
-                    )
+                    (justRemindersToday == true)
+                    ?
+                        <RemindersToday/>
+                    :
+                        <AllReminders/>
                 }
             </div>
 
