b09eb57d133d678340739a9e1585d7945d97d80c
Jess Dam <Jess Dam>
2019-11-14 14:03:10 +0000

    added styling to main, enabled all setting s from previous app, and added files for firebase set up

--- src/Main.js
+++ src/Main.js
@@ -4,8 +4,9 @@ import './Main.css'
 function Main(){
     return(
         <div className='main-box'>
-        <h1 className='main-box-content'>REMIND ME</h1>
-        <h3 className='main-box-content'>A dashboard to manage your daily life reminders</h3>
+            <h1 className='main-box-content main-title'>REMIND ME</h1>
+            <h3 className='main-box-content'>A dashboard to manage your daily life reminders</h3>
+            <img src='https://media.giphy.com/media/xTiTnqYu8LBGO2xlEA/source.gif' className='running-tiger' alt='running tiger animation'/>
         </div>
 
     )
